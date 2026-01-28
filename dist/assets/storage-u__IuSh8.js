a3102e27e2773ab59470772040ec7c7b95cf5f53
ShaikMohammedSameer3903 <2300030621@kluniversity.in>
2026-01-28 17:48:22 +0530

    fix: remove jspdf override to resolve build issues

---
+++ dist/assets/storage-u__IuSh8.js
@@ -0,0 +1,1 @@
+function n(r,t){try{const e=localStorage.getItem(r);return e?JSON.parse(e):t}catch{return t}}function o(r,t){try{localStorage.setItem(r,JSON.stringify(t))}catch{}}export{n as l,o as s};
